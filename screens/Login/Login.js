9ae0af04c81792492b33c23be715e67d67a25e86
Jean Grijp <40359722+JeanGrijp@users.noreply.github.com>
2021-12-15 08:28:41 -0300

    .

---
+++ screens/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { LoginContainer } from './style'
+
+export default function Login() {
+  return (
+    <View>
+      <LoginContainer>
+        <Text>Hi there</Text>
+      </LoginContainer>
+    </View>
+  )
+}
---
+++ screens/Login/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+
+export const LoginContainer = styled.View`
+  background-color: #c4c4;
+  width: 90%;
+  height: 10px;
+`;
